84498f2ca45242f2e0615ce2711b8ec9c8e00279
hamish <hmcc0009@student.monash.edu>
2023-04-20 11:30:16 +1000

    V1 of error prob plots

--- PSK.m
+++ PSK.m
@@ -1,4 +1,4 @@
-% Parameters
+%% Define Parameters
 M_values = [2, 4, 8];
 fc = 100e3;
 Eb = 1;
@@ -7,6 +7,7 @@ T = 0.1e-3;
 fs = 10 * fc;
 Ts = 1 / fs;
 
+%% Simulate PSK signals using randomly generated bits
 % Number of bits
 n = 1024;
 
@@ -31,21 +32,22 @@ loops = 0;
 for M = M_values
     loops = loops + 1;
     % Pad bits with zeros until the length is divisible by log2(M)
+    % If we don't do this we get an error
     bits_padded = bits;
     while mod(length(bits_padded), log2(M)) ~= 0
         bits_padded = [bits_padded, 0];
     end
     
-    % Convert bits to M-ary symbols
+    % Convert the bits to M-ary symbols
     symbols = bits_to_symbols(bits_padded, M);
 
-    % Create M-PSK waveform
+    % Create the M-PSK waveform
     waveform_segments = {};
     for symbol = symbols(1:10)
         waveform_segments{end + 1} = psk_waveform(symbol, M, fc, Es(loops), T, Ts);
     end
 
-    % Plot waveform
+    % Plot the waveform
     figure;
     hold on;
     time_vector = (0:Ts:(T * 10) - Ts);
@@ -59,3 +61,38 @@ for M = M_values
     ylabel('Amplitude');
     grid on;
 end
+
+%% Calculate error probabilities
+% Define error functions
+syms erfc_probFunc(t) erfc(x) Q(x)
+erfc_probFunc(t) = exp(-t.^2);
+erfc(x) = 2/sqrt(pi)*int(erfc_probFunc,x,Inf);
+Q(x) = (1/2)*erfc(x/sqrt(2));
+
+% Define Parameters
+Eb_N0 = 10.^((0:10)/10);
+
+% 2PSK error
+error_2PSK_bit = Q(sqrt(2.*Eb_N0));
+error_2PSK_sym = error_2PSK_bit;
+
+% 4PSK error
+error_4PSK_bit = Q(sqrt(2.*Eb_N0));
+error_4PSK_sym = erfc(sqrt(Eb_N0));
+
+% 8PSK error
+error_8PSK_sym = 2.*Q(sqrt(2.*Eb_N0.*log2(8)).*sin(pi/8));
+error_8PSK_bit = error_8PSK_sym/log2(8);
+
+figure
+hold on
+loglog(Eb_N0,error_2PSK_bit,'r-')
+loglog(Eb_N0,error_2PSK_sym,'r--')
+loglog(Eb_N0,error_4PSK_bit,'g-')
+loglog(Eb_N0,error_4PSK_sym,'g--')
+loglog(Eb_N0,error_8PSK_bit,'b-')
+loglog(Eb_N0,error_8PSK_sym,'b--')
+title('Plot of M-PSK error probabilities for bits and symbols');
+xlabel('Eb/N0 (SNR)');
+ylabel('Probability of errors');
+grid on;
